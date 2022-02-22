acfeec8ad7c5fe0a56fd30b83c801dc3410df89c
mickyle47 <72421649+mickyle47@users.noreply.github.com>
2022-02-22 18:59:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require ("body-parser");
 const request = require ("request");
 
 const app = express();
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+//app.use(express.static("public"));
+//app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
      res.sendFile(__dirname + "/index.html");     
