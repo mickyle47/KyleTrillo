a1e292bc944906096bae9d3be4a670013621bcf2
mickyle47 <72421649+mickyle47@users.noreply.github.com>
2022-02-22 19:00:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const app = express();
 //app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
-     res.sendFile(__dirname + "/index.html");     
+     res.sendFile("/index.html");     
 });
 
-app.listen(3000, function(){
-     console.log("Server started at port 3000")
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log("Server started on port 3000");
+});;
 
 
